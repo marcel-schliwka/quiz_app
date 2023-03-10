ab8d95cd8b390c7e2bd2d5a9f38dc54d61190f40
Marcel Schliwka <marcelschliwka@protonmail.com>
2023-03-10 10:29:56 +0100

    added all questions in app.js

--- app.js
+++ app.js
@@ -22,111 +22,111 @@ const questions = [
     },
     right_answer: "Os Capitulum",
   },
-  // {
-  //   question: "Wie viele Knochen hat der menschliche Körper?",
-  //   answers: {
-  //     1: "196",
-  //     2: "219",
-  //     3: "198",
-  //     4: "206",
-  //   },
-  //   right_answer: "206",
-  // },
-  // {
-  //   question: "Wie groß ist das menschliche Auge?",
-  //   answers: {
-  //     1: "14mm",
-  //     2: "23mm",
-  //     3: "12mm",
-  //   },
-  //   right_answer: "14mm",
-  // },
-  // {
-  //   question:
-  //     "Richtig oder Falsch? Die Zunge ist der einzige Muskeln, welchen man frei bewegen kann?",
-  //   answers: {
-  //     1: "Richtig",
-  //     2: "Falsch",
-  //   },
-  //   right_answer: "Richtig",
-  // },
-  // {
-  //   question: "Wie viele rote Blutkörperchen bildet der Mensch an einem Tag?",
-  //   answers: {
-  //     1: "20.000",
-  //     2: "1.000",
-  //     3: "Das hängt von der Blutmenge im Körper ab.",
-  //     4: "ungefähr 120",
-  //     5: "200 Milliarden",
-  //   },
-  //   right_answer: "200 Milliarden",
-  // },
-  // {
-  //   question: "Wie viele Rippenpaare hat ein Mensch?",
-  //   answers: {
-  //     1: "8",
-  //     2: "12",
-  //     3: "16",
-  //     4: "7",
-  //   },
-  //   right_answer: "12",
-  // },
-  // {
-  //   question: "Was zeichnet einen Menschen mit Polyaktilie aus?",
-  //   answers: {
-  //     1: "Er ist stumm.",
-  //     2: "Er hat mehr als 10 Finger.",
-  //     3: "Er hat nur eine Niere.",
-  //   },
-  //   right_answer: "Er hat mehr als 10 Finger.",
-  // },
-  // {
-  //   question: "Wie viele Blutgruppen gibt es bei einem Menschen?",
-  //   answers: {
-  //     1: "3",
-  //     2: "5",
-  //     3: "4",
-  //   },
-  //   right_answer: "4",
-  // },
-  // {
-  //   question: "Wer hat die Anatomie erfunden?",
-  //   answers: {
-  //     1: "Andreas Vesalius",
-  //     2: "Johan Wolfgang von Goethe",
-  //     3: "Es war keine einzelne Person, sondern eine Gruppe an Wissenschaftlern.",
-  //     4: "Monika Krohwinkel",
-  //   },
-  //   right_answer: "Andreas Vesalius",
-  // },
-  // {
-  //   question: "Was ist das wichtigste Organ des menschlichen Körpers?",
-  //   answers: {
-  //     1: "das Gehirn",
-  //     2: "das Herz",
-  //     3: "die Leber",
-  //   },
-  //   right_answer: "das Gehirn",
-  // },
-  // {
-  //   question: "Wie groß ist die Oberfläche der Lunge?",
-  //   answers: {
-  //     1: "20-30qm",
-  //     2: "100-140qm",
-  //     3: "200qm",
-  //   },
-  //   right_answer: "100-140qm",
-  // },
-  // {
-  //   question: "Wie oft schlägt das Herz am Tag?",
-  //   answers: {
-  //     1: "20.000x",
-  //     2: "200.000x",
-  //     3: "100.000x",
-  //     4: "24.000x",
-  //   },
-  //   right_answer: "100.000x",
-  // },
+  {
+    question: "Wie viele Knochen hat der menschliche Körper?",
+    answers: {
+      1: "196",
+      2: "219",
+      3: "198",
+      4: "206",
+    },
+    right_answer: "206",
+  },
+  {
+    question: "Wie groß ist das menschliche Auge?",
+    answers: {
+      1: "14mm",
+      2: "23mm",
+      3: "12mm",
+    },
+    right_answer: "14mm",
+  },
+  {
+    question:
+      "Richtig oder Falsch? Die Zunge ist der einzige Muskeln, welchen man frei bewegen kann?",
+    answers: {
+      1: "Richtig",
+      2: "Falsch",
+    },
+    right_answer: "Richtig",
+  },
+  {
+    question: "Wie viele rote Blutkörperchen bildet der Mensch an einem Tag?",
+    answers: {
+      1: "20.000",
+      2: "1.000",
+      3: "Das hängt von der Blutmenge im Körper ab.",
+      4: "ungefähr 120",
+      5: "200 Milliarden",
+    },
+    right_answer: "200 Milliarden",
+  },
+  {
+    question: "Wie viele Rippenpaare hat ein Mensch?",
+    answers: {
+      1: "8",
+      2: "12",
+      3: "16",
+      4: "7",
+    },
+    right_answer: "12",
+  },
+  {
+    question: "Was zeichnet einen Menschen mit Polyaktilie aus?",
+    answers: {
+      1: "Er ist stumm.",
+      2: "Er hat mehr als 10 Finger.",
+      3: "Er hat nur eine Niere.",
+    },
+    right_answer: "Er hat mehr als 10 Finger.",
+  },
+  {
+    question: "Wie viele Blutgruppen gibt es bei einem Menschen?",
+    answers: {
+      1: "3",
+      2: "5",
+      3: "4",
+    },
+    right_answer: "4",
+  },
+  {
+    question: "Wer hat die Anatomie erfunden?",
+    answers: {
+      1: "Andreas Vesalius",
+      2: "Johan Wolfgang von Goethe",
+      3: "Es war keine einzelne Person, sondern eine Gruppe an Wissenschaftlern.",
+      4: "Monika Krohwinkel",
+    },
+    right_answer: "Andreas Vesalius",
+  },
+  {
+    question: "Was ist das wichtigste Organ des menschlichen Körpers?",
+    answers: {
+      1: "das Gehirn",
+      2: "das Herz",
+      3: "die Leber",
+    },
+    right_answer: "das Gehirn",
+  },
+  {
+    question: "Wie groß ist die Oberfläche der Lunge?",
+    answers: {
+      1: "20-30qm",
+      2: "100-140qm",
+      3: "200qm",
+    },
+    right_answer: "100-140qm",
+  },
+  {
+    question: "Wie oft schlägt das Herz am Tag?",
+    answers: {
+      1: "20.000x",
+      2: "200.000x",
+      3: "100.000x",
+      4: "24.000x",
+    },
+    right_answer: "100.000x",
+  },
 ];
 
 // Global Variable
